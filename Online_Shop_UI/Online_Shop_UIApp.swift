673cbfd2ac64cf706476e91331ff0f07417c4657
Stanley Pan <imoment.developer@gmail.com>
2022-01-08 12:11:26 -0500

    Initial Commit

---
+++ Online_Shop_UI/Online_Shop_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_Shop_UIApp.swift
+//  Online_Shop_UI
+//
+//  Created by Stanley Pan on 2022/01/08.
+//
+
+import SwiftUI
+
+@main
+struct Online_Shop_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
