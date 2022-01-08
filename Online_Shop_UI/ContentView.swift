0f8787d9e1329c949549010b0e4ee7f7eb0cd8bc
Stanley Pan <imoment.developer@gmail.com>
2022-01-08 12:46:26 -0500

    Added assets, fonts, and started building TopBarView / TagLineView UI

--- Online_Shop_UI/ContentView.swift
+++ Online_Shop_UI/ContentView.swift
@@ -9,12 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("E-commerce Shop Prototype")
+        ZStack {
+            Color("background")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading) {
+                TopBarView()
+                
+                TagLineView()
+                    .padding()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TopBarView: View {
+    var body: some View {
+        HStack {
+            Button(action: { }, label: {
+                Image("menu")
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10.0)
+            })
+            
+            Spacer()
+            
+            Button(action: { }, label: {
+                Image("psyduck")
+                    .resizable()
+                    .frame(width: 42, height: 42)
+                    .cornerRadius(10.0)
+            })
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct TagLineView: View {
+    var body: some View {
+        Text("Find The \nBest ")
+            .font(.custom("PlayfairDisplay-Regular", size: 28))
+            .foregroundColor(Color("primary"))
+        + Text("Furniture!")
+            .font(.custom("PlayfairDisplay-Bold", size: 28))
+            .foregroundColor(Color("primary"))
     }
 }
