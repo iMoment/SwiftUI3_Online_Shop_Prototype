f45e5b6010671f241944ddde639334cc9f6b4ab5
Stanley Pan <imoment.developer@gmail.com>
2022-01-09 14:49:06 -0500

    Organize folders, added ProductDetailView from extracted ContentView code

--- Online_Shop_UI/ContentView.swift
+++ Online_Shop_UI/ContentView.swift
@@ -12,20 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            ZStack {
-                Color("background")
-                    .edgesIgnoringSafeArea(.all)
-                
-                VStack {
-                    Image("chair_1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .edgesIgnoringSafeArea(.top)
-                    
-                    DescriptionView()
-                        .offset(y: -40)
-                }
-            }
+            ProductDetailView()
         }
     }
 }
@@ -33,74 +20,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct DescriptionView: View {
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text("Luxury Swedian \nChair")
-                .font(.title).bold()
-            
-            HStack(spacing: 4) {
-                ForEach(0 ..< 5) { _ in
-                    Image("star")
-                }
-                
-                Text("(4.9)")
-                    .opacity(0.5)
-                    .padding(.leading, 8)
-                
-                Spacer()
-            }
-            
-            Text("Description")
-                .fontWeight(.medium)
-                .padding(.vertical, 8)
-            
-            Text("Luxury Swedian Chair is a contemporary chair based on the virtues of modern craft. It carries on the simplicity and honesty of the archetypical chair.")
-                .lineSpacing(8.0)
-                .opacity(0.6)
-            
-            HStack(alignment: .top) {
-                VStack(alignment: .leading) {
-                    Text("Size")
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 4)
-                    
-                    Text("Height: 120 cm")
-                        .opacity(0.6)
-                    
-                    Text("Width: 80 cm")
-                        .opacity(0.6)
-                    
-                    Text("Diameter: 72cm")
-                        .opacity(0.6)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                
-                VStack(alignment: .leading) {
-                    Text("Treatment")
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 4)
-                    
-                    Text("Jati Wood, Canvas, \nAmazing Love")
-                        .opacity(0.6)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            .padding(.vertical)
-            
-            HStack {
-                VStack {
-                    Text("Colors")
-                        .fontWeight(.semibold)
-                }
-            }
-        }
-        .padding()
-        .padding(.top)
-        .background(Color("background"))
-        .cornerRadius(40.0)
     }
 }
---
+++ Online_Shop_UI/ProductDetail/ProductDetailView.swift
@@ -0,0 +1,188 @@
+//
+//  ProductDetailView.swift
+//  Online_Shop_UI
+//
+//  Created by Stanley Pan on 2022/01/09.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        ZStack {
+            Color("background")
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView {
+                Image("chair_1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .edgesIgnoringSafeArea(.top)
+                
+                DescriptionView()
+                    .offset(y: -40)
+            }
+            
+            HStack {
+                Text("$1299")
+                    .font(.title)
+                    .foregroundColor(Color.white)
+                
+                Spacer()
+                
+                Button(action: { }, label: {
+                    Text("Add to Cart")
+                        .padding()
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(10.0)
+                        .foregroundColor(Color("primary"))
+                })
+            }
+            .padding()
+            .padding(.horizontal)
+            .background(Color("primary"))
+            .cornerRadius(50, corners: .topLeft)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        return clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+    }
+}
+
+struct DescriptionView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Luxury Swedian \nChair")
+                .font(.title).bold()
+            
+            HStack(spacing: 4) {
+                ForEach(0 ..< 5) { _ in
+                    Image("star")
+                }
+                
+                Text("(4.9)")
+                    .opacity(0.5)
+                    .padding(.leading, 8)
+                
+                Spacer()
+            }
+            
+            Text("Description")
+                .fontWeight(.medium)
+                .padding(.vertical, 8)
+            
+            Text("Luxury Swedian Chair is a contemporary chair based on the virtues of modern craft. It carries on the simplicity and honesty of the archetypical chair.")
+                .lineSpacing(8.0)
+                .opacity(0.6)
+            
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text("Size")
+                        .fontWeight(.semibold)
+                        .padding(.bottom, 4)
+                    
+                    Text("Height: 120 cm")
+                        .opacity(0.6)
+                    
+                    Text("Width: 80 cm")
+                        .opacity(0.6)
+                    
+                    Text("Diameter: 72cm")
+                        .opacity(0.6)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                VStack(alignment: .leading) {
+                    Text("Treatment")
+                        .fontWeight(.semibold)
+                        .padding(.bottom, 4)
+                    
+                    Text("Jati Wood, Canvas, \nAmazing Love")
+                        .opacity(0.6)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(.vertical)
+            
+            HStack(alignment: .bottom) {
+                VStack(alignment: .leading) {
+                    Text("Colors")
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        ColorDotView(color: .white)
+                        ColorDotView(color: .black)
+                        ColorDotView(color: Color("primary"))
+                    }
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    Text("Quantity")
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        Button(action: { }, label: {
+                            Image(systemName: "minus")
+                                .padding(.all, 8)
+                        })
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 50).stroke()
+                        )
+                        .foregroundColor(Color.black)
+                        
+                        Text("1")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .padding(.horizontal, 8)
+                        
+                        Button(action: { }, label: {
+                            Image(systemName: "plus")
+                                .padding(.all, 8)
+                        })
+                        .background(Color("primary"))
+                        .clipShape(Circle())
+                        .foregroundColor(Color.white)
+                    }
+                }
+            }
+        }
+        .padding()
+        .padding(.top)
+        .background(Color("background"))
+        .cornerRadius(40.0)
+    }
+}
+
+struct ColorDotView: View {
+    let color: Color
+    var body: some View {
+        color
+            .frame(width: 24, height: 24)
+            .clipShape(Circle())
+    }
+}
